45376bd14c7cac4f84b01b4f62929e98a1f3584d
Abdullah Aziz <abdullahazees4@gmail.com>
2023-09-05 02:16:53 +0500

    changes added

--- src/app/api/me/route.ts
+++ src/app/api/me/route.ts
@@ -8,6 +8,7 @@ export async function GET(request: NextRequest) {
   try {
     const userID = await getDataFromToken(request);
     const userData = await User.findOne({ _id: userID }).select("-password");
+    console.log(userData);
     return NextResponse.json({ message: "User found", data: userData });
   } catch (error: any) {
     return NextResponse.json({ error: error.message, status: 500 });
