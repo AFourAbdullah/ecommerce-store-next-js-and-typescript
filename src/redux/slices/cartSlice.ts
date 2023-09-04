e94714d4d5cc010b5e590587d7f0a60ab82b9ec2
Abdullah Aziz <abdullahazees4@gmail.com>
2023-09-04 22:50:10 +0500

    code added

--- src/redux/slices/cartSlice.ts
+++ src/redux/slices/cartSlice.ts
@@ -16,7 +16,7 @@ interface Image {
 }
 import { createSlice, PayloadAction } from "@reduxjs/toolkit";
 // const initialState: any =  ;
-const initialCartItems = localStorage.getItem("cartItems");
+const initialCartItems = window.localStorage.getItem("cartItems");
 
 const cartSlice = createSlice({
   name: "cart",
@@ -37,13 +37,13 @@ const cartSlice = createSlice({
         // If item doesn't exist, add it to the cart
         state.cartItems.push(newItem);
       }
-      localStorage.setItem("cartItems", JSON.stringify(state.cartItems));
+      window.localStorage.setItem("cartItems", JSON.stringify(state.cartItems));
     },
     remove(state, action) {
       state.cartItems = state.cartItems.filter(
         (item: any) => item._id !== action.payload
       );
-      localStorage.setItem("cartItems", JSON.stringify(state.cartItems));
+      window.localStorage.setItem("cartItems", JSON.stringify(state.cartItems));
     },
   },
 });
