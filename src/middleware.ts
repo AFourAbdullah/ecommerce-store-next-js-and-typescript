58c73c2ec5d2ff8fc5c2ca96aae14399f4aabf98
Abdullah Aziz <abdullahazees4@gmail.com>
2023-09-06 02:53:28 +0500

    files modified

--- src/middleware.ts
+++ src/middleware.ts
@@ -3,12 +3,14 @@ import type { NextRequest } from "next/server";
 // import { cookies } from 'next/headers'
 
 import { getDataFromToken } from "./utils/getDataFromToken";
+import Cookies from "js-cookie";
 
 export async function middleware(request: NextRequest) {
   const path = request.nextUrl.pathname;
   const isPublicPath = path == "/login" || path == "/signup";
-  const token = request.cookies.get("token")?.value || "";
-  
+  // const token = request.cookies.get("token")?.value || "";
+  const token = Cookies.get("token");
+
   if (isPublicPath && token != "") {
     return NextResponse.redirect(new URL(`/profile`, request.nextUrl));
   }
@@ -17,6 +19,7 @@ export async function middleware(request: NextRequest) {
     return NextResponse.redirect(new URL("/login", request.nextUrl));
   }
 }
+
 export const config = {
   matcher: [
     "/profile",
