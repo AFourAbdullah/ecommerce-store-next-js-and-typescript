091f9c100d1b1428aa3c3ceabc63eaef42b76f73
Abdullah Aziz <abdullahazees4@gmail.com>
2023-09-06 01:41:00 +0500

    files added

--- src/utils/getDataFromToken.ts
+++ src/utils/getDataFromToken.ts
@@ -1,10 +1,11 @@
 import { NextRequest } from "next/server";
 import jwt from "jsonwebtoken";
-
+import { cookies } from "next/headers";
 export const getDataFromToken = (request: NextRequest) => {
   try {
     // const token = request.cookies.get("token")?.value || "";
-    const token = request.cookies.get("token")?.value || "";
+    const cookieStore = cookies();
+    const token = cookieStore.get("token")?.value || "";
     console.log("tokennssss: ", token);
     const decodedToken: any = jwt.verify(token, "abcdefgh91");
     return decodedToken.id;
