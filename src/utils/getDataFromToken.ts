dc107d65061693190a1796ca281152534df52340
Abdullah Aziz <abdullahazees4@gmail.com>
2023-09-06 00:47:08 +0500

    files added

--- src/utils/getDataFromToken.ts
+++ src/utils/getDataFromToken.ts
@@ -5,7 +5,7 @@ export const getDataFromToken = (request: NextRequest) => {
   try {
     // const token = request.cookies.get("token")?.value || "";
     const token = request.cookies.get("token")?.value || "";
-    const decodedToken: any = jwt.verify(token, process.env.TOKEN_SECRET!);
+    const decodedToken: any = jwt.verify(token, "abcdefgh91");
     return decodedToken.id;
   } catch (error: any) {
     throw new Error(error.message);
